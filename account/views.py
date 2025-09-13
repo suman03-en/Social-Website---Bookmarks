e8e1fee6dbabf4b53ba3632af19a969f24197423
suman phuyal <sfuyal59@gmail.com>
2025-09-13 18:21:48 +0545

    first commit

---
+++ account/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+def dashboard(request):
+    return render(
+        request,
+        'account/dashboard.html',
+        {
+            'section':'dashboard'
+        }
+    )
