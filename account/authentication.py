56be7399b913a46979eff7ffc3309b29cdb60357
suman phuyal <sfuyal59@gmail.com>
2025-09-14 14:57:43 +0545

    Auto profile created for social auth user

--- account/authentication.py
+++ account/authentication.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from .models import Profile
 
 class EmailAuthBackend:
     """
@@ -17,4 +18,8 @@ class EmailAuthBackend:
         try:
             return User.objects.get(pk=user_id)  
         except User.DoesNotExist:
-            return None 
+            return None
+
+def create_profile(backend,user,*args,**kwargs):
+    """Create user profile for social authentication"""
+    Profile.objects.get_or_create(user=user)
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -112,6 +112,19 @@ AUTHENTICATION_BACKENDS = [
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('GOOGLE_OAUTH2_KEY')
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('GOOGLE_OAUTH2_SECRET')
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
@@ -128,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
