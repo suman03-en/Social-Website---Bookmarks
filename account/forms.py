a636cb28d72e11a7d68a657da5cc629c0189cd01
suman phuyal <sfuyal59@gmail.com>
2025-09-14 10:54:13 +0545

    Unique email validation during registration

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from .models import Profile
 
 class LoginForm(forms.Form):
@@ -26,7 +27,13 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError("Passwords don't match.")
         return cd['password2']
-
+    
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError("Email already in use.")
+        return data
+    
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
