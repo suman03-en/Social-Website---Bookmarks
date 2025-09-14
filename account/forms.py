54aed24660d38c0f4d17c254e0362b94ccdcb8d9
suman phuyal <sfuyal59@gmail.com>
2025-09-14 11:02:42 +0545

    Unique email check in usereditform

--- account/forms.py
+++ account/forms.py
@@ -39,6 +39,18 @@ class UserEditForm(forms.ModelForm):
         model = get_user_model()
         fields = ['first_name','last_name','email']
 
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        qs = User.objects.exclude(
+            id=self.instance.id
+        ).filter(
+            email=data
+        )
+        if qs.exists():
+            raise forms.ValidationError('Email already in use.')
+        
+        return data
+
 class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
