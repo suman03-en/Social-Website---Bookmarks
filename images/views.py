4a01136885a4960503cfa42c407a63155ef1e5dc
suman phuyal <sfuyal59@gmail.com>
2025-09-16 14:19:55 +0545

    infinite scrolling using js requests

--- images/views.py
+++ images/views.py
@@ -1,8 +1,9 @@
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect,render,get_object_or_404
 from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from .models import Image
 from .forms import ImageCreateForm
 
@@ -67,3 +68,30 @@ def image_like(request):
             pass
     
     return JsonResponse({'status':'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images,8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(
+            request,
+            'images/image/list_images.html',
+            {'section':'images','images':images}
+        )
+    return render(
+        request,
+        'images/image/list.html',
+        {'section':'images','images':images}
+    )
